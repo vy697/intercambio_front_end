f90496bf64acdbd5ff97b26cb341048f003411e6
Vy Nguyen <vy.p.ngn@gmail.com>
2016-09-22 10:37:53 -0600

    added localized 'results for' on search results, messages are displayed at appropriate times, suggestions for users are retrieved upon logging in, strings for user home page have been interpolated, forms (landing, search and signup) have validations

--- js/controllers.js
+++ js/controllers.js
@@ -19,10 +19,14 @@ app.controller('landingController', ['logoutService', 'userService', 'loginServi
 
   //on button clicks ng-click
   vm.changeLanguage = localizeService.changeLanguage;
+
+
   //retrieve list of all users
-  vm.getExchanges = searchService.getExchanges;
+  // vm.getExchanges = searchService.getExchanges;
   //list of all users
-  vm.exchanges = searchService.exchanges;
+  // vm.exchanges = searchService.exchanges;
+
+
   //log user in
   vm.login = loginService.login;
   //if a user is logged in, retrieve their info
@@ -38,10 +42,12 @@ app.controller('landingController', ['logoutService', 'userService', 'loginServi
 
   //list of user's language exchange matches
   vm.searchResults = searchService.searchResults;
+
+  vm.errorMessage = '';
+
   //ng-click to retrieve matches based on user inputs
   vm.findMatches = function(i_speak, i_learn, city) {
     var lang_preference;
-
     if($window.sessionStorage.lang_preference) {
       lang_preference = $window.sessionStorage.lang_preference;
     //request here with whatever lang_preference is in localStorage
@@ -50,16 +56,33 @@ app.controller('landingController', ['logoutService', 'userService', 'loginServi
     } else {
       lang_preference = 'en';
     }
-    $http.get('http://localhost:3000/search/results', {params:{"i_speak": i_speak, "i_learn": i_learn, "city": city, "lang_preference": lang_preference}})
-    .then(function(data) {
-      console.log('findMatches on landing: ', data);
-      searchService.searchResults.data = data.data;
-      $location.url('/search');
-    })
-    .catch(function(err) {
-      console.log('findMatches err on landing: ', err);
-    });
+    if(vm.form.$valid) {
+      $http.get('http://localhost:3000/search/results', {params:{"i_speak": i_speak, "i_learn": i_learn, "city": city, "lang_preference": lang_preference}})
+      .then(function(data) {
+        console.log('findMatches on landing: ', data);
+        searchService.searchResults.data = data.data;
+
+
+        if(data.data.message === 'no matches were found') {
+          searchService.searchResults.message = localizeService.keys.no_results[lang_preference];
+        } else {
+          searchService.searchResults.data = data.data;
+          searchService.searchResults.message = localizeService.keys.yes_results[lang_preference];
+        }
+        console.log('searchResults on landing: ', searchService.searchResults);
+      })
+      .then(function() {
+        $location.url('/search');
+      })
+      .catch(function(err) {
+        console.log('findMatches err on landing: ', err);
+      });
+    } else {
+      //render validation message from lang keys object on localizeService
+      vm.errorMessage = vm.keys.err[lang_preference];
+    }
   };
+
 
   //retrieve cities upon going to view
   vm.getCities = searchService.getCities();
@@ -74,7 +97,7 @@ app.controller('signupController', ['localizeService', 'searchService', '$http',
 
   //set language keys are stored here
   vm.keys = localizeService.keys;
-  vm.key = localizeService.key;
+
   //localize page based on user's lang setting
   localizeService.localizeForUser();
 
@@ -85,36 +108,55 @@ app.controller('signupController', ['localizeService', 'searchService', '$http',
   //retrieve cities upon going to view
   vm.getCities = searchService.getCities();
 
+
+  vm.errorMessage = '';
+
   vm.submit = function(name, email, password, city, description, age, photo_url, pair, group, online, lang_preference, i_speak, i_learn, i_speak_level, i_learn_level) {
     console.log(vm.name, vm.email, vm.password, vm.city, vm.description, vm.age, vm.photo_url, vm.pair, vm.group, vm.online, vm.lang_preference, vm.i_speak, vm.i_speak_level, vm.i_learn, vm.i_learn_level);
-    return $http.post('http://localhost:3000/signup', {
-      name: name,
-      email: email,
-      pw: password,
-      city: city,
-      description: description,
-      age: age,
-      photo_url: photo_url,
-      pair: pair,
-      group: group,
-      online: online,
-      lang_preference: lang_preference,
-      user_speaks: {
-        language_id: i_speak,
-        level_id: i_speak_level
-      },
-      user_learns: {
-        language_id: i_learn,
-        level_id: i_learn_level
+
+    //set language to display form validations in
+    var language = '';
+    if(localStorage.lang_preference) {
+        language = localStorage.lang_preference;
+      } else {
+        language = 'en';
       }
-    })
-    .then(function(result) {
-      console.log('ANGULAR SIGNUP RESULT: ', result);
-    })
-    .catch(function(err) {
-      err.statusText = "Unable to sign up";
-      console.log('SIGNUP ERROR:', err);
-    });
+
+    //if the form meets the set requirements, send it through to be posted
+    if(vm.form.$valid) {
+      return $http.post('http://localhost:3000/signup', {
+        name: name,
+        email: email,
+        pw: password,
+        city: city,
+        description: description,
+        age: age,
+        photo_url: photo_url,
+        pair: pair,
+        group: group,
+        online: online,
+        lang_preference: lang_preference,
+        user_speaks: {
+          language_id: i_speak,
+          level_id: i_speak_level
+        },
+        user_learns: {
+          language_id: i_learn,
+          level_id: i_learn_level
+        }
+      })
+      .then(function(result) {
+        console.log('ANGULAR SIGNUP RESULT: ', result);
+      })
+      .catch(function(err) {
+        err.statusText = "Unable to sign up";
+        console.log('SIGNUP ERROR:', err);
+      });
+      //otherwise display validation messages
+    } else {
+      vm.errorMessage = vm.keys.form_validate[language];
+      console.log(vm.errorMessage);
+    }
   };
 }]);
 
@@ -126,11 +168,11 @@ app.controller('profileController', [function() {
 
 }]);
 
-app.controller('homeController', ['userService', 'localizeService', function(userService, localizeService) {
+app.controller('homeController', ['searchService', 'userService', 'localizeService', function(searchService, userService, localizeService) {
 
   var vm = this;
 
-  //if a user is logged in, retrieve their info
+  //if a user is logged in, retrieve their info TODO uncomment if everything breaks
   vm.getUserInfo = userService.getUserInfo();
 
   //user information for logged in user saved here
@@ -138,6 +180,11 @@ app.controller('homeController', ['userService', 'localizeService', function(use
 
   //localize page based on user's lang setting
   localizeService.localizeForUser();
+
+  //list of logged in user's matches stored here
+  vm.searchResults = searchService.searchResults;
+
+  vm.findMatchesForUser = searchService.findMatchesForUser;
 
 }]);
 
@@ -156,7 +203,7 @@ app.controller('indexController', ['logoutService', function(logoutService) {
 
 }]);
 
-app.controller('searchController', ['searchService', 'localizeService', function(searchService, localizeService) {
+app.controller('searchController', ['$window', '$http', 'searchService', 'localizeService', function($window, $http, searchService, localizeService) {
 
   var vm = this;
 
@@ -165,19 +212,54 @@ app.controller('searchController', ['searchService', 'localizeService', function
   //list of cities are stored here to be accessed in the view in the select box
   vm.cityList = searchService.cityList;
 
-  //list of user's language exchange matches
-  vm.searchResults = searchService.searchResults;
-
-  //ng-click to retrieve matches based on user inputs
-  vm.findMatches = searchService.findMatches;
-
   //set language keys are stored here
   vm.keys = localizeService.keys;
 
   //localize page based on user's lang setting
   localizeService.localizeForUser();
-  //language keys are stored here
-  vm.keys = localizeService.keys;
-  vm.key = localizeService.key;
+
+  //ng-click to retrieve matches based on user inputs
+  // vm.findMatches = searchService.findMatches;
+  vm.searchResults = searchService.searchResults;
+
+  vm.errorMessage = '';
+
+  vm.findMatches = function(i_speak, i_learn, city) {
+
+    console.log('findMatches invoked', i_speak, i_learn, city);
+    var lang_preference;
+
+    if($window.sessionStorage.lang_preference) {
+      lang_preference = $window.sessionStorage.lang_preference;
+    //request here with whatever lang_preference is in localStorage
+  } else if (localStorage.lang_preference) {
+      lang_preference = localStorage.lang_preference;
+    } else {
+      lang_preference = 'en';
+    }
+    if(vm.form.$valid) {
+    vm.errorMessage = '';
+    $http.get('http://localhost:3000/search/results', {params:{"i_speak": i_speak, "i_learn": i_learn, "city": city, "lang_preference": lang_preference}})
+    .then(function(data) {
+      console.log('initial searchResults data:' , data);
+      console.log('lang_preference: ', lang_preference);
+
+      if(data.data.message === 'no matches were found') {
+        vm.searchResults.data = {};
+        vm.searchResults.message = localizeService.keys.no_results[lang_preference];
+      } else {
+        //set result list to empty if new search doesn't return anything
+        vm.searchResults.data = data.data;
+        vm.searchResults.message = localizeService.keys.yes_results[lang_preference];
+      }
+    })
+    .catch(function(err) {
+      console.log('search results err: ', err);
+    });
+  } else {
+    //render validation message from lang keys object on localizeService
+    vm.errorMessage = vm.keys.err[lang_preference];
+  }
+};
 
 }]);
--- js/services.js
+++ js/services.js
@@ -1,17 +1,72 @@
 'use strict';
 
-app.service('localizeService', ['$translate', '$window', function($translate, $window){
+app.service('localizeService', ['$location', '$translate', '$window', function($location, $translate, $window){
 
   var sv = this;
 
   //store lang keys here
   sv.keys = {
     en: 'en',
-    es: 'es'
+    es: 'es',
+    err: {
+      en: 'please fill out all the fields!',
+      es: 'favor de rellenar todos los campos!'
+    },
+    yes_results: {
+      en: 'your search results: ',
+      es: 'los resultados de tu búsqueda: '//+Spanish exchanges in city
+    },
+    no_results: {
+      en: 'Ahh shucks. No results were returned for your search',
+      es: '¡Maldita sea! No hay resultados para tu búsqueda.'
+    },
+    form_validate: {
+      en: {
+        form_invalid: 'Please fill out the required fields *',
+        form_valid: 'Sweet. All good to go.',
+        name_required: 'Your name is required!',
+        name_length: 'You must have more than two letters...',
+        email_required: 'Email is required.',
+        email_valid: 'Please include a valid email',
+        pw_required: 'A password is required',
+        pw_length: 'Password needs to contain: 6 alphanumeric characters minimum, at least one number, no special characters',
+        pw_retype: 'Please retype your pw',
+        pw_no_match: 'Passwords don\'t match!',
+        pw_maxlength: 'Password can\'t be longer than 20 characters',
+        city: 'Tell us where you want to find your exchange.',
+        photo_url: 'Not a valid photo_url',
+        photo_url_length: 'Your url is too loooooong',
+        lang_preference: 'What language do you want your account to be in?',
+        i_speak: 'Tell us what language you speak.',
+        lang_level: 'Don\'t forget about the level!',
+        i_learn: 'What language do you want to learn?'
+
+      },
+      es: {
+        form_invalid: 'Por favor, rellena los campos requeridos *',
+        form_valid: 'Perfecto. List@s para empezar.',
+        name_required: 'Hay que poner el nombre!',
+        name_length: 'El nombre tiene que contener como mínimo dos letras...',
+        email_required: 'El email es necesario',
+        email_valid: 'Por favor, introduce un email válido',
+        pw_required: 'Vas a necesitar una contraseña...',
+        pw_length: 'La contraseña tiene que contener: como mínimo 6 caracteres alfanuméricos, al menos un dígito, y ningún caracter especial',
+        pw_retype: 'Por favor, vuelve a introducir la contraseña',
+        pw_no_match: '¡Las contraseñas no son iguales!',
+        pw_maxlength: 'La contraseña no puede contener más de 20 caracteres',
+        city: 'Dinos dónde quieres buscar tu intercambio.',
+        photo_url: 'El enlace no es válido',
+        photo_url_length: 'El enlace tiene que ser más corto',
+        lang_preference: '¿En qué idioma quieres que sea la cuenta?',
+        i_speak: 'Dinos el idioma que hablas.',
+        lang_level: '¡No te olvides del nivel!',
+        i_learn: '¿Qué idioma quieres aprender?'
+      }
+    }
+
   };
 
   sv.localizeForUser = function(key) {
-
     console.log('localStorage', localStorage.lang_preference);
     //user's language settings determine the language of the session
     if($window.sessionStorage.token) {
@@ -31,12 +86,13 @@ app.service('localizeService', ['$translate', '$window', function($translate, $w
     $translate.use(key);
     //when default lang settings change, store it in local storage so that the setting persists
     localStorage.setItem('lang_preference', key);
+    $window.location.href = '/';
   };
 
 }]);
 
 
-app.service('searchService', ['$http', '$window', function($http, $window) {
+app.service('searchService', ['localizeService', '$http', '$window', function(localizeService, $http, $window) {
 
   var sv = this;
 
@@ -63,48 +119,50 @@ app.service('searchService', ['$http', '$window', function($http, $window) {
       });
     };
 
+  //data of all users stored here
   sv.exchanges = {};
 
-  sv.getExchanges = function() {
-    var lang_preference;
+  //TODO: what to do with get all exchanges???
+  // sv.getExchanges = function() {
+  //   var lang_preference;
+  //
+  //   if($window.sessionStorage.lang_preference) {
+  //     lang_preference = $window.sessionStorage.lang_preference;
+  //   //request here with whatever lang_preference is in localStorage
+  //   } else if (localStorage.lang_preference) {
+  //     lang_preference = localStorage.lang_preference;
+  //   } else {
+  //     lang_preference = 'en';
+  //   }
+  //   $http.get("http://localhost:3000/search", {params:{"lang_preference": lang_preference}})
+  //   .then(function(data) {
+  //     sv.exchanges.data = data.data;
+  //     console.log('THIS IS THE EXCHANGE DATA: ', sv.exchanges);
+  //   })
+  //   .catch(function(err) {
+  //     console.log('where is my data: ', err);
+  //   });
+  // };
+  sv.keys = localizeService.keys;
 
-    if($window.sessionStorage.lang_preference) {
-      lang_preference = $window.sessionStorage.lang_preference;
-    //request here with whatever lang_preference is in localStorage
-    } else if (localStorage.lang_preference) {
-      lang_preference = localStorage.lang_preference;
-    } else {
-      lang_preference = 'en';
-    }
-    $http.get("http://localhost:3000/search", {params:{"lang_preference": lang_preference}})
-    .then(function(data) {
-      sv.exchanges.data = data.data;
-      console.log('THIS IS THE EXCHANGE DATA: ', sv.exchanges);
-    })
-    .catch(function(err) {
-      console.log('where is my data: ', err);
-    });
-  };
-
+ // data (list of matches) from findMatches stored here.
   sv.searchResults = {};
 
-  sv.findMatches = function(i_speak, i_learn, city) {
 
-    console.log('findMatches invoked', i_speak, i_learn, city);
-    var lang_preference;
+  //for home page sugggestions list upon going to home page
+  sv.findMatchesForUser = function(i_speak, i_learn, city, lang_preference) {
 
-    if($window.sessionStorage.lang_preference) {
-      lang_preference = $window.sessionStorage.lang_preference;
-    //request here with whatever lang_preference is in localStorage
-    } else if (localStorage.lang_preference) {
-      lang_preference = localStorage.lang_preference;
-    } else {
-      lang_preference = 'en';
-    }
-    $http.get('http://localhost:3000/search/results', {params:{"i_speak": i_speak, "i_learn": i_learn, "city": city, "lang_preference": lang_preference}})
+    lang_preference = $window.sessionStorage.lang_preference;
+    i_speak = $window.sessionStorage.i_speak;
+    i_learn = $window.sessionStorage.i_learn;
+    city = $window.sessionStorage.city;
+
+    console.log('findMatchesForUser invoked', i_speak, i_learn, city, lang_preference);
+
+    return $http.get('http://localhost:3000/search/results', {params:{"i_speak": i_speak, "i_learn": i_learn, "city": city, "lang_preference": lang_preference}})
     .then(function(data) {
       sv.searchResults.data = data.data;
-      console.log('initial searchResults data:' , data);
+      console.log('initial searchResults data from findMatchesForUser:' , data);
     })
     .catch(function(err) {
       console.log('search results err: ', err);
@@ -113,7 +171,7 @@ app.service('searchService', ['$http', '$window', function($http, $window) {
 
 }]);
 
-app.service('userService', ['$http', '$window', function($http, $window) {
+app.service('userService', ['searchService', '$http', '$window', function(searchService, $http, $window) {
 
   var sv = this;
 
@@ -121,21 +179,32 @@ app.service('userService', ['$http', '$window', function($http, $window) {
 
   sv.getUserInfo = function() {
     //if a user is logged in (if a jwt exists), retrieve their info
-    if($window.sessionStorage.token) {
+    //if($window.sessionStorage.token) {
     return $http.get('http://localhost:3000/users')
     .then(function(data) {
       sv.userData.data = data.data.data[0];
+      //set user settings to grab in findMatches later
+      $window.sessionStorage.i_speak = sv.userData.data.speaks_language_id;
+      $window.sessionStorage.i_learn = sv.userData.data.learns_language_id;
+      $window.sessionStorage.city = sv.userData.data.translated_location;
+      console.log('sv.userData.data: ', sv.userData.data);
+      searchService.findMatchesForUser();
+    })
+    .then(function(data) {
+      console.log('findMatchesForUser in getUserInfoFunc data: ', data);
     })
     .catch(function(err) {
       console.log('getUserInfo err: ', err);
     });
-   }
+   //}
   };
 }]);
 
-app.service('loginService', ['$window', '$http', '$location', function($window, $http, $location) {
+app.service('loginService', ['userService', '$window', '$http', '$location', function(userService, $window, $http, $location) {
 
   var sv = this;
+
+  sv.userData = userService.userData;
 
   sv.login = function(email, password) {
      return $http.post('http://localhost:3000/auth', {
@@ -157,6 +226,7 @@ app.service('loginService', ['$window', '$http', '$location', function($window, 
       delete $window.sessionStorage.token;
     });
   };
+
 }]);
 
 app.service('logoutService', ['$window', '$location', function($window, $location) {
